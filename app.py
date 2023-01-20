b6c61f45aa9731b4d08cf7a67fe98dec94ddcfec
brstagner <111202913+brstagner@users.noreply.github.com>
2023-01-20 11:57:12 -0600

    modified for render

--- app.py
+++ app.py
@@ -32,6 +32,16 @@ app.app_context().push()
 
 connect_db(app)
 
+engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sa.inspect(engine)
+if not inspector.has_table("users"):
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        app.logger.info('Initialized the database!')
+else:
+    app.logger.info('Database already contains the users table.')
+
 ### User Routes ###
 
 @app.route('/')
